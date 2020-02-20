41436be08ece07800f8133096ef32af002a2f830
asthashrivastava <39181997+asthashrivastava@users.noreply.github.com>
2020-02-20 18:31:51 +0530

    Delete makefile.mk

--- makefile.mk
+++
@@ -1,5 +0,0 @@
-README.md: guessinggame.sh
-	touch README.md
-	echo "guessinggame.sh" > README.md
-	echo "Make File execution Date and Time $$(date)  " >> README.md
-	echo "* **No. of lines in GuessingGame.sh:** $$(wc -l guessinggame.sh | cut -d ' ' -f1)  " >> README.md
